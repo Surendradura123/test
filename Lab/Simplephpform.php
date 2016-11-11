c2cbc13559eeaf9b60e6b98c6c4c213dc1e39877
x09092943 <x09092943@student.ncirl.ie>
2016-11-11 09:04:12 -0500

    message

---
+++ Lab/Simplephpform.php
@@ -0,0 +1,24 @@
+<html>
+  <body>
+    <form action="Book.php" method="post">
+      Favorite book: <input type="text" name="title">
+      <br/>
+      Author: <input type="text" name="author">
+      <br/>
+      Genre: <input type="text" name="genre">
+      <br/>
+      Price: <input type="number" name="price">
+      <br/>
+      Publication date: <input type="date" name="date">
+      <br/>
+      Publisher: <input type="text" name="name">
+      <br/>
+      Description: <input type="text" name="desc">
+      <br/>
+      <input type="checkbox" name="inprint" value="print1">In print<br/>
+      <input type="checkbox" name="noprint" value="print2">Out of print<br/>
+      
+      <input type="submit">
+    </form>
+  </body>
+</html>
